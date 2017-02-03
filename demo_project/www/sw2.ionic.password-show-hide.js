b94fed8cba560de444f6c736587f02a64beb1589
Sonic <sw2hk-info@yahoo.com.hk>
2017-02-03 14:36:43 +0800

    Update sw2.ionic.password-show-hide.js
    v1.1 keep focus on field when click on button

--- demo_project/www/sw2.ionic.password-show-hide.js
+++ demo_project/www/sw2.ionic.password-show-hide.js
@@ -1,34 +1,60 @@
 /* ================================================================================
 https://github.com/sonicwong/ionic-password-show-hide
-v1.0.0
+v1.1.0
 ================================================================================ */
 
 angular.module('sw2.ionic.password-show-hide', [])
-    .directive('ionicPasswordShowHide', function ($compile) {
-        return {
-            restrict: 'A',
-            scope: {
-                ngModel: '='
-            },
-            controller: function ($scope, $element) {
+.directive('ionicPasswordShowHide', ["$compile", "$timeout", function ($compile, $timeout) {
+    return {
+        restrict: 'A',
+        scope: {
+            ngModel: '='
+        },
+        controller: [   "$scope", "$element", "$attrs", "ionicInputClearableConfig",
+            function ($scope, $element, $attrs, ionicInputClearableConfig) {
+                var element = $element[0];
+
                 //Parent element should be position:relative to make clear button in correct positon
                 $element.parent().css( "position", "relative" );
+
+                //Show/Hide button icon class name
+                $scope.btnShowClass = element.hasAttribute("data-password-show-btn-class") ? element.getAttribute("data-password-show-btn-class") : 'ion-eye';
+                $scope.btnHideClass = element.hasAttribute("data-password-hide-btn-class") ? element.getAttribute("data-password-hide-btn-class") : 'ion-eye-disabled';
                 
-                $scope.showHideBtnIcon = 'ion-eye-disabled';
-                
-                $scope.clearBtn = angular.element('<a tabindex="-1" ng-cloak ng-class="showHideBtnIcon" class="password-show-hide-btn button button-icon icon" ng-click="togglePasswordField()"></a>');
-                $compile($scope.clearBtn)($scope);
-                $element.after($scope.clearBtn);
-                
-                $scope.togglePasswordField = function () {
+                //Default to hide password at start
+                $scope.btnClass = $scope.btnHideClass;
+
+                //ng-hide class used to fix init flash problem
+                $scope.btn = angular.element('<a tabindex="-1" ng-cloak ng-class="btnClass" class="password-show-hide-btn button button-icon icon" ng-click="btnClick()"></a>');
+                $compile($scope.btn)($scope);
+                $element.after($scope.btn);
+
+                $scope.btnClick = function () {
                     if( $element.attr('type') == 'password' ){
                         $element.attr('type', 'text');
-                        $scope.showHideBtnIcon = 'ion-eye';
+                        $scope.btnClass = $scope.btnShowClass;
                     }else{
                         $element.attr('type', 'password');
-                        $scope.showHideBtnIcon = 'ion-eye-disabled';
+                        $scope.btnClass = $scope.btnHideClass;
                     }
+
+                    //keep focus on the field when click on the button
+                    function preventBluring() {
+                        element.focus();
+                        $element.unbind( 'blur', preventBluring );
+                    }
+                    $element.bind( 'blur' , preventBluring );
                 }
+
+                function refresh() {
+                    $timeout( function () {
+                        $scope.$digest();
+                    });
+                }
+
+                $element.bind( 'focus', refresh );
+                $element.bind( 'blur', refresh );
             }
-        }
-    })
+        ]
+    }
+}]);
